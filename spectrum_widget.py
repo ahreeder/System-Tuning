d5bbf72a5de9c199ae680ee30b326d1b827f1790
PA Tuning Tool <dev@pa-tuning-tool>
2026-03-12 16:23:05 -0400

    Widen Y-axis to -90 to 0 dB and hide auto-range button

--- spectrum_widget.py
+++ spectrum_widget.py
@@ -16,10 +16,11 @@ class SpectrumWidget(pg.PlotWidget):
         self.setLabel('bottom', 'Frequency (Hz)')
         self.setLabel('left', 'Level (dB)')
         self.getViewBox().disableAutoRange()
-        self.setYRange(-60, 12, padding=0)
+        self.setYRange(-90, 0, padding=0)
         self.setXRange(np.log10(20), np.log10(20000))
         self.showGrid(x=True, y=True, alpha=0.2)
         self.setMouseEnabled(x=False, y=False)
+        self.getPlotItem().hideButtons()
 
         ax = self.getAxis('bottom')
         ax.setTicks([[(np.log10(f), _freq_label(f)) for f in FREQ_TICKS]])
@@ -34,7 +35,7 @@ class SpectrumWidget(pg.PlotWidget):
 
     def update_live(self, freqs: np.ndarray, db: np.ndarray):
         self._live.setData(np.log10(freqs), db)
-        self.setYRange(-60, 12, padding=0)
+        self.setYRange(-90, 0, padding=0)
 
     def set_target(self, freqs: np.ndarray, db: np.ndarray):
         self._target.setData(np.log10(freqs), db)
